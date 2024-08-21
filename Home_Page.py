3c05f87ac24568a89e7a9ff1461de6927eca4f27
Cynthia Siewtu <mcynsiew@gmail.com>
2024-08-21 15:45:40 +0200

    ADD: Why section to Home Page

--- Home_Page.py
+++ Home_Page.py
@@ -13,15 +13,22 @@ def page_home():
     st.markdown('<h1 style="color: #CAE85D;">Nebenan</h1>', unsafe_allow_html=True)
     st.markdown("""<h3 style="color: #FE9DE1;">Streamlit Learning Session</h3>""", unsafe_allow_html=True)
     st.markdown("""
-            Streamlit: An open-source Python library that makes it easy to create interactive web apps for data science and machine learning projects.
+            *Streamlit*: An open-source Python library that makes it easy to create interactive web apps for data science and machine learning projects.
+
+            ## Why streamlit?
+            - **Bridging the Gap:** Enables data scientists to build web apps without needing web developers.
+            - **Simplifying Insight Sharing:** Move beyond static reports to dynamic, interactive apps.
+            - **Rapid Prototyping:** Quickly create and test ideas with instant feedback.
+            - **Lowering the Barrier to Entry:** Accessible to a wider audience, including non-programmers.
+            - **Fostering Collaboration:** Encourages sharing and collaboration within the community.
 
             ## Key Features
 
             - **Simple**: Build web apps with just a few lines of Python.
             - **Interactive Widgets**: Easily add sliders, buttons, etc.
             - **Real-time Updates**: Apps update automatically with code or data changes.
-            - **Data Visualization**: Integrates with visualisation packages such as Matplotlib, Seaborn, Plotly.
-            - **Easy Deployment**: Share apps with a link using Streamlit Cloud.
+            - **Data Visualization**: Integrates with visualisation packages such as Matplotlib, Seaborn and Plotly.
+            - **Easy Deployment**: Share apps with a link using Streamlit Cloud, whether they're standalone or connected to Snowflake for data integration.
 
             ## Example: Simple App
 
